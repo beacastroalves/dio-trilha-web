e1bdef8b787bc443d593b4414f9c01453517dfd9
Beatriz Castro <beacastro1008@gmail.com>
2024-12-03 19:14:33 +0000

    manipulando multiplas requisicoes

--- dio-javascript/aula06/pokedex/assets/js/poke-api.js
+++ dio-javascript/aula06/pokedex/assets/js/poke-api.js
@@ -1,10 +1,18 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailsRequests) => Promise.all(detailsRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
+
 }
